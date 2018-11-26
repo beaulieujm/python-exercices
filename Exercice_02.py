ffd4b3772f349e59427223ae8e5e3a1feedd8380
jmbeaulieu <jmbeaulieu@jmbeaulieu.com>
2018-11-26 06:17:39 +0000

    test

--- Exercice_02.py
+++ Exercice_02.py
@@ -1,1 +1,29 @@
 # pratice-python exercice #2 
+# Ask the user for a number. Depending on whether the number is even or odd, 
+# print out an appropriate message to the user.
+
+def get_a_number(number) :
+	try :
+		nombre = int(number)
+	except ValueError:
+		print("Input is not a valid number.  Try again")
+		nombre = "Not a number"
+	return nombre
+	
+is_done = False	
+while not is_done : 
+	input_string  = input("Enter a number (q to exit) : ")
+	if input_string == "q" :
+		is_done = True
+		break
+	nombre = get_a_number(input_string)
+	if nombre != "Not a number" :
+		if nombre%2 == 0 :
+	  		parite = "pair"
+		else :
+	  		parite = "impair"    
+		print("Le nombre " + repr(nombre) + " est " + parite + ".")
+
+print("All done!")
+exit() 
+
